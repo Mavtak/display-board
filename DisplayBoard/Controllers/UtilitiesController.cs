e4e87bf545c2defe1b79d86023876907b121d6e8
David McGrath <github.com@davidmcgrath.com>
2013-10-09 17:30:40 -0400

    added GetContentType action to the Utilities

--- DisplayBoard/Controllers/UtilitiesController.cs
+++ DisplayBoard/Controllers/UtilitiesController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Web.Mvc;
 
 namespace DisplayBoard.Controllers
 {
@@ -9,5 +12,19 @@ namespace DisplayBoard.Controllers
             return View((object)url);
         }
 
+        public ActionResult GetContentType(string url)
+        {
+            var request = WebRequest.Create(url) as HttpWebRequest;
+
+            // hehehe so sneaky
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36";
+
+            using (var response = request.GetResponse())
+            {
+                var contentType = response.ContentType??string.Empty;
+
+                return Content(contentType);
+            }
+        }
     }
 }
